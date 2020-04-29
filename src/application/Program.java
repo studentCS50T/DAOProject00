9e1226bcfe1b1054ec2ce47ddc8f54fb603489b1
studentCS50T <timoteodp10@gmail.com>
2020-04-29 15:14:13 -0300

    Seller Addeed

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,12 +1,18 @@
 package application;
 
+import java.util.Date;
+
 import model.entities.Department;
+import model.entities.Seller;
 
 public class Program {
 
 	public static void main(String[] args) {
 
 		Department obj = new Department(1, "Books");
+		Seller seller = new Seller(21, "Bob", "bob@outlook.com", new Date(),300.0, obj);
+		
 		System.out.println(obj);
+		System.out.println(seller);
 	}
 }
